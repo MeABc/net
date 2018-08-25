2eb8bcdf447df94019251738c9cfa1d9599bc661
Ruslan Nigmatullin <elessar@dropbox.com>
2018-08-24 17:32:08 -0700

    http2: don't leak streams on broken body

--- http2/transport.go
+++ http2/transport.go
@@ -1089,6 +1089,7 @@ func (cc *ClientConn) roundTrip(req *http.Request) (res *http.Response, gotErrAf
 			default:
 			}
 			if err != nil {
+				cc.forgetStreamID(cs.ID)
 				return nil, cs.getStartedWrite(), err
 			}
 			bodyWritten = true
@@ -1210,6 +1211,7 @@ func (cs *clientStream) writeRequestBody(body io.Reader, bodyCloser io.Closer) (
 			sawEOF = true
 			err = nil
 		} else if err != nil {
+			cc.writeStreamReset(cs.ID, ErrCodeCancel, err)
 			return err
 		}
 
